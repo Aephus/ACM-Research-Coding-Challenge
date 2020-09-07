23c4aa4487cbc5463f40dd903cc1b6e5a5c04029
aephus <aaron.chang384@gmail.com>
2020-09-07 01:25:05 -0500

    updated plot display

--- dbscan.py
+++ dbscan.py
@@ -8,18 +8,16 @@ import seaborn as sns
 
 df = pd.read_csv('ClusterPlot.csv', header=0)
 df = df[['V1', 'V2']]
-x = df['V1']
-y = df['V2']
-
-plt.plot(x, y, 'ro')
 
 db = DBSCAN(min_samples=3).fit(df)
 cluster_labels = db.labels_
 
 n_clusters = len(set(cluster_labels)) - (1 if -1 in cluster_labels else 0)
 df['cluster_labels'] = cluster_labels
-print(cluster_labels)
+print('There are', n_clusters, 'clusters.')
 
-plot = sns.relplot(x='V1', y='V2', hue=cluster_labels, data=df)
-#plot.legend(title='n cluster')
+g = sns.relplot(x='V1', y='V2', hue=cluster_labels, data=df, legend='full')
+legend = g._legend
+legend.set_title('nth cluster')
+
 plt.show()
--- kmeans.py
+++ kmeans.py
@@ -5,13 +5,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.set(style='darkgrid')
+
 
 df = pd.read_csv('ClusterPlot.csv', header=0)
 df = df[['V1', 'V2']]
 
-# plt.plot(x, y, 'ro')
-# plt.show()
 silhouette_scores = []
 
 for n_clusters in range(2, 8):
@@ -23,11 +21,13 @@ for n_clusters in range(2, 8):
 
 max_score = max(silhouette_scores, key=lambda i: i[1])
 n_clusters = max_score[0]
-print(n_clusters)
+print('There are', n_clusters, 'clusters.')
 
 clusterer = KMeans(n_clusters=n_clusters, init='k-means++')
 cluster_labels = clusterer.fit_predict(df)
 
-plot = sns.relplot(x='V1', y='V2', hue=cluster_labels, data=df)
-#plot.legend(title='n cluster')
+g = sns.relplot(x='V1', y='V2', hue=cluster_labels, data=df, legend='full')
+legend = g._legend
+legend.set_title('nth cluster')
+
 plt.show()
